ccd878f86ebcb0a8501a0db58a1b1c7516b0c245
Talha <talha.chughtai@outlook.com>
2026-06-03 10:56:47 -0400

    Set TEST_MODE to true
    Changed TEST_MODE to true for testing purposes.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { parse } = require("csv-parse/sync");
 const fs = require("fs");
 
 // SET THIS TO true FOR TESTING, THEN BACK TO false WHEN YOU ARE DONE
-const TEST_MODE = false;
+const TEST_MODE = true;
 
 // Pulls the secure webhook URL from GitHub's hidden environment variables
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
